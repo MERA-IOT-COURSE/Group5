afe1c65ec8d662c86bc5ffbd6943249b986d176b
Your Name <you@example.com>
2018-11-14 12:30:40 +0300

    Fixes

--- client/index.js
+++ client/index.js
@@ -1,30 +1,18 @@
 var mqtt = require('mqtt');
 
 let URL = "mqtt://10.42.0.10:1883";
-let TOPIC_NAME = "ivan;"
+let TOPIC_NAME = "ivan";
 
-let options = {
-  clientId:"00000000f7be3be2"
-};
+let options = {};
 
 var client = mqtt.connect(URL, options);
 
-client.on('connect', () => {
-  console.log("Connected");
-  client.subscribe(
-    TOPIC_NAME,
-    {},
-    (err) => {
-      if (!err) {
-        client.publish(TOPIC_NAME, "Hello, world!");
-      }
-    });
-});
+client.on('connect', () => client.subscribe(TOPIC_NAME, options));
 
 client.on('message', (topic, message) => {
-  //if(topic === TOPIC_NAME) {
-    console.log("Publish handling, message: " + message);
-  //}
+  if (topic == TOPIC_NAME) {
+    console.log("Message received: " + message);
+  }
 });
 
 client.on('error', error => console.log(error));
