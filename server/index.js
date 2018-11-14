174b0d291726ef11dbde188a741d93d40c830c08
Your Name <you@example.com>
2018-11-14 11:20:05 +0300

    added listen call

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const parser = require('body-parser');
 
+const port = 3000;
 const app = express();
 
 app.get('/testendpoint', (req, res) => {
   res.send("Hello, world!");
   return;
-})
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
