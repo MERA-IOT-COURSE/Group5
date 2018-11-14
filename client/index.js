84a4a95eb89b9dd3140c62e45a087816ab0349d7
Your Name <you@example.com>
2018-11-14 11:38:03 +0300

    Trying to connet to broker

--- client/index.js
+++ client/index.js
@@ -1,7 +1,20 @@
 var mqtt=require('mqtt');
 
 let URL = "mqtt://10.42.0.10:1883";
+let TOPIC_NAME = "ivan;"
+
 let options = {
   clientId:"00000000f7be3be2"
 };
-var client = mqtt.connect(URL,options);
+
+var client = mqtt.connect(URL, options);
+
+client.on('connect', () => {
+  client.subscribe(TOPIC_NAME);
+});
+
+client.on('message', (topic, message) => {
+  //if(topic === TOPIC_NAME) {
+    console.log(message == null ? "Default message" : message);
+  //}
+});
--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var mqtt=require('mqtt');
-
-let URL = "mqtt://10.42.0.10:1883";
-let options = {
-  clientId:"00000000f7be3be2"
-};
-var client = mqtt.connect(URL,options);
+//stub
